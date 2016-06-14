2e7ce3e0f6804dea1ec75705319799232ae64fab
Jackymancs4 <jackyman_cs4@live.it>
2016-06-14 21:57:29 +0200

    Linted and indented code.

--- ana-test1.php
+++ ana-test1.php
@@ -1,20 +1,19 @@
 <?php
+
 ini_set('memory_limit', '512M');
 set_time_limit(0);
 
-include("class/ana-class.php");
+include 'class/ana-class.php';
 
 $anamorph = new Anamorph();
-$anamorph->load("img/moustache2.jpg");
+$anamorph->load('img/moustache2.jpg');
 
-$altezzacm=5;
-$lunghezzaoriginecm=10;
-$altezzaoriginecm=20;
+$altezzacm = 5;
+$lunghezzaoriginecm = 10;
+$altezzaoriginecm = 20;
 
 $anamorph->init($lunghezzaoriginecm, $altezzaoriginecm, $altezzacm);
 $anamorph->make_anamorph_fill();
-$anamorph->make_ana_perspective_fill("add");
+$anamorph->make_ana_perspective_fill('add');
 
 $anamorph->return_image();
-
-?>
--- ana-test2.php
+++ ana-test2.php
@@ -1,20 +1,19 @@
 <?php
+
 ini_set('memory_limit', '512M');
 set_time_limit(0);
 
-include("class/ana-class.php");
+include 'class/ana-class.php';
 
 $anamorph = new Anamorph();
-$anamorph->load("img/aaa.jpg");
+$anamorph->load('img/aaa.jpg');
 
-$altezzacm=5; 
-$lunghezzaoriginecm=10;
-$altezzaoriginecm=20;
+$altezzacm = 5;
+$lunghezzaoriginecm = 10;
+$altezzaoriginecm = 20;
 
-$anamorph->init($lunghezzaoriginecm, $altezzaoriginecm, $altezzacm); 
+$anamorph->init($lunghezzaoriginecm, $altezzaoriginecm, $altezzacm);
 $anamorph->make_anamorph();
 $anamorph->make_ana_perspective();
 
 $anamorph->return_image();
-
-?>
